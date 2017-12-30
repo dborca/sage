8117e156eeaff3535cb33c961b3f1c879eece268
sezero <sezero@users.sourceforge.net>
2017-12-30 17:01:00 +0300

    workaround the SSE movaps issue in DJGPP DXE builds:
    the workaround is bound to a new BROKEN_MOVAPS define in nasm flags.

--- x86/sse_vertex.asm
+++ x86/sse_vertex.asm
@@ -28,7 +28,7 @@ proc	sse_Vertex3fv, 4
 	shufps	xmm0, [f_0001], SHUF(X, Y, Z, W)
 	shufps	xmm0, xmm0, SHUF(Y, Z, X, W)
 	movlps	xmm0, [eax]
-	movaps	[edx], xmm0
+	MOVUAPS	[edx], xmm0
 	cmp	ecx, [tnl_vb + TNL_VB_MAX]
 	je	.0
 	ret
@@ -50,7 +50,7 @@ proc	sse_Color3fv, 4
 	shufps	xmm0, [f_0001], SHUF(X, Y, Z, W)
 	shufps	xmm0, xmm0, SHUF(Y, Z, X, W)
 	movlps	xmm0, [edx]
-	movaps	[eax], xmm0
+	MOVUAPS	[eax], xmm0
 	mov	eax, [tnl_vb + TNL_VB_FLAGS]
 	or	dword [eax + ecx * 4], TNL_COLOR0_BIT
 	or	dword [tnl_general_flags], TNL_COLOR0_BIT
@@ -66,7 +66,7 @@ proc	sse_Color4fv, 4
 	sal	eax, 4
 	add	eax, [tnl_vb + TNL_VB_COLOR0_PTR]
 	movups	xmm0, [edx]
-	movaps	[eax], xmm0
+	MOVUAPS	[eax], xmm0
 	mov	eax, [tnl_vb + TNL_VB_FLAGS]
 	or	dword [eax + ecx * 4], TNL_COLOR0_BIT
 	or	dword [tnl_general_flags], TNL_COLOR0_BIT
@@ -81,9 +81,9 @@ proc	sse_TexCoord2fv, 4
 	mov	eax, ecx
 	sal	eax, 4
 	add	eax, [tnl_vb + TNL_VB_TEXCOORD0_PTR]
-	movaps	xmm0, [f_0001]
+	MOVUAPS	xmm0, [f_0001]
 	movlps	xmm0, [edx]
-	movaps	[eax], xmm0
+	MOVUAPS	[eax], xmm0
 	mov	eax, [tnl_vb + TNL_VB_FLAGS]
 	or	dword [eax + ecx * 4], TNL_TEXCOORD0_BIT
 	or	dword [tnl_general_flags], TNL_TEXCOORD0_BIT
@@ -102,9 +102,9 @@ proc	sse_MultiTexCoord2fv, 8
 	mov	eax, ebx
 	sal	eax, 4
 	add	eax, [tnl_vb + edx * 4 - GL_TEXTURE0 * sizeof_TNL_ARRAY + TNL_VB_TEXCOORD0_PTR]
-	movaps	xmm0, [f_0001]
+	MOVUAPS	xmm0, [f_0001]
 	movlps	xmm0, [esi]
-	movaps	[eax], xmm0
+	MOVUAPS	[eax], xmm0
 	mov	edx, [tnl_vb + TNL_VB_FLAGS]
 	sub	ecx, GL_TEXTURE0
 	mov	eax, TNL_TEXCOORD0_BIT
